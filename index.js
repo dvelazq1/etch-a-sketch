9bf270f2e9eed5f6b1e9fde5d308b71aae26766c
Diana Velazquez Ramos <dvelazq1@uncc.edu>
2022-05-29 14:18:06 -0400

    Add menu

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
+// set mousedown equal to false
 let mouseDown = false
 document.body.onmousedown = () => (mouseDown = true)
 document.body.onmouseup = () => (mouseDown = false)
 
 let color = "#A0785A";
-
+// change each individual cell color as the mouse hovers over them
 function changeColor(e) {
     e.target.style.backgroundColor = color;
 }
 const container = document.getElementById("container");
 var gridDivs = [];
-let num = 16; 
-for (i=0; i <16; i++){
+let num = 16;
+
+for (i=0; i <num; i++){
     const newRow = document.createElement("div");
     newRow.id = "newRow"
     container.appendChild(newRow);
@@ -21,7 +23,6 @@ gridDivs.forEach(newRow => {
     for (i=0; i <num; i++){
         const newDiv = document.createElement("div");
         newDiv.id = "gridDiv"
-        
         newDiv.addEventListener('mouseover', changeColor);
         newRow.appendChild(newDiv);
     }
