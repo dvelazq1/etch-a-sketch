2e4e27f47043d6f95c679a06c1fcbb8f611a4a35
Diana Velazquez Ramos <dvelazq1@uncc.edu>
2022-05-30 10:18:52 -0400

    set up color picker

--- index.js
+++ index.js
@@ -3,29 +3,52 @@ let mouseDown = false
 document.body.onmousedown = () => (mouseDown = true)
 document.body.onmouseup = () => (mouseDown = false)
 // set up defaults
-let color = "#A0785A";
+let color = "#E7C283";
 let num = 16;
 const container = document.getElementById("container");
 var gridDivs = [];
 
-for (i=0; i <num; i++){
-    const newRow = document.createElement("div");
-    newRow.id = "newRow"
-    container.appendChild(newRow);
-    gridDivs.push(newRow);
-}
-
-gridDivs.forEach(newRow => {
+function createGrid(number){
+    number = num;
     for (i=0; i <num; i++){
-        const newDiv = document.createElement("div");
-        newDiv.id = "gridDiv"
-        newDiv.addEventListener('mouseover', changeColor);
-        newRow.appendChild(newDiv);
+        const newRow = document.createElement("div");
+        newRow.id = "newRow"
+        container.appendChild(newRow);
+        gridDivs.push(newRow);
+    }
+
+    gridDivs.forEach(newRow => {
+        for (i=0; i <num; i++){
+            const newDiv = document.createElement("div");
+            newDiv.id = "gridDiv"
+            newDiv.addEventListener('mouseover', changeColor);
+            newRow.appendChild(newDiv);
     }
 });
+}
+
+createGrid();
 
 // change each individual cell color as the mouse hovers over them
 function changeColor(e) {
     e.target.style.backgroundColor = color;
 }
 
+document.getElementById("gridSizeText").addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+      createGrid(num);
+    }
+});
+
+document.getElementById("colorSelection").addEventListener("input", function( event ) {
+    // highlight the mouseleave target
+    color = this.value;
+});
+
+
+document.getElementById("clearGridButton").onclick = clearGrid;
+
+
+function clearGrid() {
+    console.log("bue")
+}
