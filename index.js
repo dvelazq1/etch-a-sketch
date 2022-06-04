cbf913bf49f0e390d66e6e4be0027f16820201d8
Diana Velazquez Ramos <dvelazq1@uncc.edu>
2022-06-04 16:03:57 -0400

    Fix Grid Size Input

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-// set mousedown equal to false
-let mouseDown = false
-document.body.onmousedown = () => (mouseDown = true)
-document.body.onmouseup = () => (mouseDown = false)
 // set up defaults
 let color = "#E7C283";
-let num = 16;
+let newGrid = false;
+let num = 0;
+
 const container = document.getElementById("container");
 var gridDivs = [];
 
-function createGrid(number){
-    number = num;
+function createGrid(){
+    if (num === 0 && newGrid === false){
+        num = 16;
+    }
     for (i=0; i <num; i++){
         const newRow = document.createElement("div");
         newRow.id = "newRow"
@@ -27,19 +27,31 @@ function createGrid(number){
 });
 }
 
-createGrid();
+function createNewGrid(number) {
+    document.getElementById("container").innerHTML="";
+    newGrid = true;
+    num = number;
+    createGrid();
+
+}
 
 // change each individual cell color as the mouse hovers over them
 function changeColor(e) {
     e.target.style.backgroundColor = color;
 }
 
-document.getElementById("gridSizeText").addEventListener('keypress', function (e) {
+document.getElementById("gridSizeText").addEventListener('keydown', function (e) {
     if (e.key === 'Enter') {
-      createGrid(num);
+        if (isNaN(this.value) || this.value < 1 || this.value > 100 )
+        {
+          alert("Must input numbers between 1 and 100");
+          return false;
+        }
+        e.preventDefault();
+        createNewGrid(this.value);
+        return false;
     }
 });
-
 // change color values based on user input
 document.getElementById("colorSelection").addEventListener("input", function( event ) {
     color = this.value;
@@ -51,4 +63,8 @@ document.getElementById("clearGridButton").onclick = clearGrid;
 // clear the grid but keep the same dimensions
 function clearGrid() {
     document.querySelectorAll('.gridDiv').forEach(div => div.style.backgroundColor = "white");
+}
+
+if (!newGrid){
+createGrid();
 }
